966f679870b59b9891d68a291f722e42f90ba273
suriyakanth1 <suriya271999@gmail.com>
2021-12-06 17:01:52 +0530

    mathfunction,date and time

---
+++ src/com/beginner/udemy/mathfunction/MathFunctionsApp.java
@@ -0,0 +1,22 @@
+package com.beginner.udemy.mathfunction;
+
+public class MathFunctionsApp {
+
+    public static void main(String[] args) {
+
+        double temperatureYesterday = 5.9;
+        double temperatureToday = -Math.random() * 10;
+
+        System.out.println("Absolute temperature today: " + Math.abs(temperatureToday));
+        System.out.println("Min: " + Math.min(temperatureToday, temperatureYesterday));
+        System.out.println("Max: " + Math.max(temperatureToday, temperatureYesterday));
+        System.out.println("Round: " + Math.round(temperatureToday));
+        System.out.println("Ceiling: " + Math.ceil(temperatureYesterday));
+        System.out.println("Floor: " + Math.floor(temperatureYesterday));
+
+
+        System.out.println("Power: " + Math.pow(temperatureToday, 2));
+        System.out.println("Square root: " + Math.sqrt(Math.abs(temperatureToday)));
+
+    }
+}
